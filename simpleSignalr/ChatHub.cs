d5ecfb7fc0e011409505486614d40ca3e323779a
Sukmuang Setthawut - Together TH <s.sukmuang@togetherteam.co.th>
2017-12-29 11:53:01 +0700

    simple context success

---
+++ simpleSignalr/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace simpleSignalr
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void ShowAllClient(string name)
+        {
+            string[] a = new string[10];
+            a[1] = Clients.Caller.userName;
+            a[0] = name;
+            Clients.All.clientOnline(name);
+        }
+
+        public void RealtimeData() {
+            //Random sampleRandom = new Random();
+            // int number = sampleRandom.Next(1000);
+            int number = 1; 
+            Clients.All.realtime(number);
+        }
+    }
+}
